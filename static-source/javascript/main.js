ca74faa761377f6f37330c5f10c52742ee83750a
Miguel Fondeur <miguelfondeur@gmail.com>
2018-03-27 18:23:59 -0400

    add tithe.ly link to homepage

--- static-source/javascript/main.js
+++ static-source/javascript/main.js
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:55bb6af47fb6cb591ec0050182726e84d6fed01542de45b9bab9093dc6af31fd
-size 730
+oid sha256:6a2fde7865757bdbddd6180be92a06a533d521bbd449696f52dda51618df14e1
+size 842
